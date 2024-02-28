9937f6c14bdf429169050f1dca4ac369d5029eaa
Aswin Krishna <116379127+TheProv1@users.noreply.github.com>
2024-02-28 20:41:27 +0400

    Update and rename perfect_square.py to Python/PerfectSquare.py

--- Python/PerfectSquare.py
+++ Python/PerfectSquare.py
@@ -1,16 +1,8 @@
 """
+Perfect Square
+
 Given a positive integer num, return true if num is a perfect square or false otherwise.
 A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
-
-Input: num = 16
-Output: true
-
-Input: num = 14
-Output: false
-
-Constraints:
-
-1 <= num <= 231 - 1
 """
 
 class Solution:
