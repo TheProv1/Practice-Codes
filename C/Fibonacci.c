06e38177f0f6a5e0d56ce2c83d3f330f3059015b
Aswin Krishna <116379127+TheProv1@users.noreply.github.com>
2024-02-28 20:55:59 +0400

    Update Fibonacci.c

--- C/Fibonacci.c
+++ C/Fibonacci.c
@@ -1,7 +1,8 @@
 /*
 Fibonacci
 
-The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1  
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, 
+such that each number is the sum of the two preceding ones, starting from 0 and 1  
   */
 
 int fib(int n){
