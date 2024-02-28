dc926eed86440eb1805528c3a41b33ca12cf0a20
Aswin Krishna <116379127+TheProv1@users.noreply.github.com>
2024-02-28 20:39:45 +0400

    Create Fibo.c

---
+++ Fibo.c
@@ -0,0 +1,21 @@
+/*
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1  
+  */
+
+int fib(int n){
+    int num1 = 0, num2 = 1;
+    int num3 = 0;
+
+    if (n == 1){
+        return num2;
+    }
+
+    while (n > 1){
+        num3 = num1 + num2;
+        num1 = num2;
+        num2 = num3;
+        n--;
+    }
+
+    return num3;
+}
