d2f63213c8d029ffc57c18323b6a53e2a9e17daf
Aswin Krishna <116379127+TheProv1@users.noreply.github.com>
2024-02-28 20:55:48 +0400

    Update Fibonacci.java

--- Java/Fibonacci.java
+++ Java/Fibonacci.java
@@ -1,3 +1,11 @@
+/*
+Fibonacci
+
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, 
+such that each number is the sum of the two preceding ones, starting from 0 and 1
+*/
+
+
 class Solution {
     public int fib(int n) {
         int num1 = 0;
