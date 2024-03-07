325caced03fce5863fa8d5ca4614b1aa4824a549
Aswin Krishna <116379127+TheProv1@users.noreply.github.com>
2024-03-07 14:41:29 +0400

    Update ArticleViewsI.sql

--- SQL/ArticleViewsI.sql
+++ SQL/ArticleViewsI.sql
@@ -15,7 +15,6 @@ Note that equal author_id and viewer_id indicate the same person.
  
 
 Write a solution to find all the authors that viewed at least one of their own articles. Return the result table sorted by id in ascending order.
-The result format is in the following example.
 */
 
 select DISTINCT(author_id) as id from Views where viewer_id = author_id order by author_id;
